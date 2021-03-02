22fc12e5da13f708199bec0877c112ac2d56c8e3
sergioivanov008 <sergioivanov008@gmail.com>
2021-03-02 11:21:23 +0300

    feat: implement count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  let
+module.exports = function countCats(matrix) {
+  let cats = "^^";
+  let sum = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == cats) {
+        sum += 1;
+      }
+    }
+  }
+  return sum;
 };
