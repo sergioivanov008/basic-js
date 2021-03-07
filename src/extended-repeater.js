549b8684fde7d9e9ebacc87034329445ac1f4547
sergioivanov008 <sergioivanov008@gmail.com>
2021-03-07 12:09:12 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatTimes = options.repeatTimes;
+  let separator = options.separator || '+';
+  let addition = options.addition;
+  if (String(addition) == 'null') {
+    addition = String(addition)
+  }
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator || '|';
+  let stringAll = String(str);
+
+  if (addition !== undefined && additionRepeatTimes == undefined) {
+    stringAll = stringAll + addition;
+  } else if (addition !== undefined && additionRepeatTimes !== undefined) {
+    let additionStringArray = [];
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      additionStringArray.push(addition);
+    }
+    stringAll = stringAll + additionStringArray.join(additionSeparator);
+  }
+
+  if (repeatTimes !== undefined) {
+    let additionStringArray = [];
+    for (let i = 0; i < repeatTimes; i++) {
+      additionStringArray.push(stringAll);
+    }
+    stringAll = additionStringArray.join(separator);
+  }
+
+  return stringAll;
 };
-  
