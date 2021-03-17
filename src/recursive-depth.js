42622c222c4334277d467b0be66351653af07570
sergioivanov008 <sergioivanov008@gmail.com>
2021-03-17 20:24:31 +0300

    feat: implement recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,31 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let arrLevel = 1;
+    for (let i = 0; i < arr.length; i++) {
+      let nextArrLevel = 1;
+      if (Array.isArray(arr[i])) {
+        nextArrLevel += this.calculateDepth(arr[i]);
+        if (nextArrLevel > arrLevel) {
+          arrLevel = nextArrLevel;
+        }
+      }
+    }
+    return arrLevel;
   }
 };
+
+
+//let depthCalc = new DepthCalculator();
+// depthCalc.calculateDepth([1, 2, 3, 4, 5]);// => `1`
+// depthCalc.calculateDepth([1, 2, 3, [4, 5]]);// => `2`
+// depthCalc.calculateDepth([[[]]]);// => `3`
+
+// depthCalc.calculateDepth([1, 2, 3, 4, 5, [1]]);//, 2);
+// depthCalc.calculateDepth([1, 2, 3, [1], 4, 5, [1]]);//, 2);
+// depthCalc.calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]);//, 3);
+// depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]);//, 4);
+// depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]);//, 5);
+// depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]);//, 15);
+// depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]);//, 25);
+// depthCalc.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]);//, 31);
+
